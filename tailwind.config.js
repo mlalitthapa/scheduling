d350dc36e9d83f6bcd2f873b093eaf901d5d6694
Lalit Thapa <mlalitthapa@gmail.com>
2024-05-29 20:12:57 +0545

    Setup nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
