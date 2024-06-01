aa004d7a7729631ce66a4641d3ac8015a386337f
Lalit Thapa <mlalitthapa@gmail.com>
2024-06-01 17:17:25 +0545

    Add tests for search input

---
+++ __tests__/components/SearchCoach.test.tsx
@@ -0,0 +1,27 @@
+import SearchCoach from '@/components/coach/SearchCoach';
+import { render, screen, userEvent, waitFor } from '@testing-library/react-native';
+
+const CHANGED_TEXT = 'John';
+
+describe('Search input', () => {
+  const onSearch = jest.fn();
+
+  beforeEach(() => {
+    render(<SearchCoach onSearch={onSearch} />);
+  });
+
+  it('should have an input element', () => {
+    const input = screen.getByPlaceholderText('Search');
+    expect(input).toBeOnTheScreen();
+  });
+
+  it('should call onSearch function with input after 500ms', async () => {
+    const input = screen.getByPlaceholderText('Search');
+    const user = userEvent.setup();
+
+    await user.type(input, CHANGED_TEXT);
+    expect(onSearch).toHaveBeenCalledTimes(0);
+
+    await waitFor(() => expect(onSearch).toHaveBeenCalledWith(CHANGED_TEXT), { timeout: 510 });
+  });
+});
