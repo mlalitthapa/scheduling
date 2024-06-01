040aaa4310997d8a8433031e08fa182264951ac3
Lalit Thapa <mlalitthapa@gmail.com>
2024-06-01 13:48:07 +0545

    Eslint fix

--- config/queryClient.ts
+++ config/queryClient.ts
@@ -4,8 +4,8 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 5 * 60 * 1000, // Default stale time is 5 minutes
-    }
-  }
+    },
+  },
 });
 
 export default queryClient;
